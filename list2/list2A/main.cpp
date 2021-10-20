41890d7cf4797eda32bc7ea820b2d03fce3c623d
ClessonR <bebettus2009@live.com>
2021-10-20 11:48:19 -0300

    Last Stable Version

--- list2/list2A/main.cpp
+++ list2/list2A/main.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include <cmath>
 
+
+
 using namespace std;
 
-long long int binary_search(long long int *main_array, long long int l, long long int r, long long int find); //? A binary search function which receives an array, its number of elements and an item to find;
+long int binary_search(long int main_array[],long int l,long int r,long int find); //? A binary search function which receives an array, its number of elements and an item to find;
 
 int main() {
 
-    int number_elt, queries =0;
+    long int number_elt, queries =0;
 
     cin >> number_elt; //? Number of elements in array
     cin >> queries; //? Number of queries to be made;
 
-    long long int * array = new long long int [number_elt];
+    long int* array = new  long int [number_elt];
 
 
-    long long int temp;
+
     for (int i = 0; i < number_elt; ++i) { //! Populating the array;
-        cin >> temp;
-        array[i] = temp;
+        cin >> array[i];
     }
 
+    long int temp;
     for (int i = 0; i < queries ; ++i) { // ! Executing the queries
         cin >> temp;
-        cout<< binary_search(array,0,(number_elt-1),temp)<< endl;
+        cout<< binary_search(array,0,(number_elt-1),temp)<< '\n';
     }
 
     delete[] array; //? Memory Dispenser
@@ -31,12 +33,14 @@ int main() {
     return 0;
 }
 
-long long int binary_search(long long int *main_array, long long int l, long long int r, long long int find){
-    long long int m;
-    if (l > r){
+long int binary_search(long int main_array[], long int l, long int r, long int find){
+    long int m;
+    if (l > r)
+    {
         return -1;
     }
-    else{
+    else
+    {
         m = floor((l + r)/2);
 
         if(main_array[m] == find){
